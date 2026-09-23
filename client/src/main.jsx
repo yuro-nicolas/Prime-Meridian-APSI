6e9ca2c3d5cea7d60cf8495e74dbc777c3866d21
yuro <120780008+yuro-nicolas@users.noreply.github.com>
2026-09-23 20:41:45 +0800

    Add BrowserRouter for routing in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/base.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
